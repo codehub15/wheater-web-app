76f5417024dcff5910bf01d3f343fdf4b1cbf442
natalialexandra <natalia.alexandra195@gmail.com>
2020-10-11 21:41:59 +0200

    update

---
+++ build/precache-manifest.fa0fd1499859dc8448b850e897a33ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6550ad3e780b690ac460d74387cea648",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3b5bc6eeb2f4cc0a52e",
+    "url": "./static/css/main.9b89fa25.chunk.css"
+  },
+  {
+    "revision": "b752f54720134aecc915",
+    "url": "./static/js/2.4af69954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4af69954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b5bc6eeb2f4cc0a52e",
+    "url": "./static/js/main.4031f0e3.chunk.js"
+  },
+  {
+    "revision": "4fb81f3c7642eb2feec3",
+    "url": "./static/js/runtime-main.e287a196.js"
+  },
+  {
+    "revision": "051e123c2444b28b02730bf1821ffcd3",
+    "url": "./static/media/sun.051e123c.svg"
+  }
+]);
